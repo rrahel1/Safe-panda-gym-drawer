4ffcdd785f4781aec7c3ce98f58235f0e0b45772
Rahel Rickenbach <rrahel@ethz.ch>
2025-02-21 14:13:39 +0100

    change function call

--- panda_gym/envs/tasks/drawer.py
+++ panda_gym/envs/tasks/drawer.py
@@ -70,7 +70,7 @@ class Drawer(Task):
         self.drawer_body_id = get_body_unique_id(self.sim, self.drawer_name)
 
         # Set lateral rolling and spinning friction for the handle
-        self.sim.changeDynamics(self.drawer_body_id, 1, lateralFriction=1.0, rollingFriction=0.01, spinningFriction=0.01)
+        self.sim.changeDynamics(self.drawer_body_id, 1, 1.0, 0.01, 0.01)
 
 
         if self.debug:
